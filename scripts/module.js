90d66c33c29b20511fcbead86f31b132948da281
NeV3rKilL <nev3rkill@gmail.com>
2021-06-12 01:30:46 +0200

    Init push 1.0.0

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,46 @@
-Hooks.once('init', async function() {
+Hooks.once('init', async function () {
 
+  CONFIG.debug.hooks = true;
 });
 
-Hooks.once('ready', async function() {
+Hooks.once('ready', async function () {
+  CONFIG.statusEffects.push({
+    "id": "fastAction",
+    "label": "EFFECT.StatusFastAction",
+    "icon": "modules/alien-actions/assets/fast-action.svg"
+  });
 
+  CONFIG.statusEffects.push({
+    "id": "slowAction",
+    "label": "EFFECT.StatusSlowAction",
+    "icon": "modules/alien-actions/assets/slow-action.svg"
+  });
 });
+
+Hooks.on('updateCombat', async function (e) {
+  let combat_token = canvas.tokens.get(e.current.tokenId);
+  let combat_actor = combat_token.actor;
+
+  if (e.current.turn == 0) { // if we are in first round
+    e.combatants.forEach(combatant => {
+      //if (combatant.actor.type != "creature") removeActions(combatant.token);
+      removeActions(combatant.token);
+    });
+  }
+
+  if (combat_actor.type == "creature") { // if its a creature
+    if (combat_actor.data.data.attributes.speed.value > 1) {
+      removeActions(combat_token);
+    }
+  }
+});
+
+async function removeActions(combat_token) {
+
+  let slowAction = combat_token.actor.effects.find(eff => eff.getFlag("core", "statusId") === "slowAction");
+  let fastAction = combat_token.actor.effects.find(eff => eff.getFlag("core", "statusId") === "fastAction");
+
+  if (slowAction != undefined) await slowAction.delete();
+  if (fastAction != undefined) await fastAction.delete();
+
+};
